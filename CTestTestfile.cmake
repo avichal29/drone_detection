cfbb590d54cb1bc6500fc06e5f2bcf40194d6a0e
avichal29 <130253035+avichal29@users.noreply.github.com>
2024-05-17 15:09:46 +0530

    Add files via upload

---
+++ CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/avichal/ros_workspace/src
+# Build directory: /home/avichal/ros_workspace/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("yolo_detection")
---
+++ yolo_detection/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/avichal/ros_workspace/src/yolo_detection
+# Build directory: /home/avichal/ros_workspace/build/yolo_detection
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
